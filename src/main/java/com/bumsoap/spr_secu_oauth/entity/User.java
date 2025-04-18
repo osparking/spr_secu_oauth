a7d254a156a822dadd08414cb3764131d55406ce
박종범 <jbpark03@gmail.com>
2025-04-18 14:47:42 +0900

    user entity class w/ one explicit constructor

--- src/main/java/com/bumsoap/spr_secu_oauth/entity/User.java
+++ src/main/java/com/bumsoap/spr_secu_oauth/entity/User.java
@@ -1,4 +1,42 @@
 package com.bumsoap.spr_secu_oauth.entity;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role; //ADMIN, MANAGER, USER
+
+    private String provider; //어떤 OAuth인지(google, naver 등)
+    private String provideId; // 해당 OAuth 의 key(id)
+
+    private LocalDateTime createDate;
+
+    @Builder
+    public User(String username, String password, String email, Role role,
+                String provider, String provideId, LocalDateTime createDate) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.provider = provider;
+        this.provideId = provideId;
+        this.createDate = createDate;
+    }
 }
