14b831facb629950212772ddcc01caa46a888cac
박종범 <jbpark03@gmail.com>
2025-04-18 20:20:44 +0900

    prepend annotation to UserRepo interface

--- src/main/java/com/bumsoap/spr_secu_oauth/repo/UserRepo.java
+++ src/main/java/com/bumsoap/spr_secu_oauth/repo/UserRepo.java
@@ -2,9 +2,11 @@ package com.bumsoap.spr_secu_oauth.repo;
 
 import com.bumsoap.spr_secu_oauth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Long> {
     public Optional<User> findByUsername(String username);
 }
