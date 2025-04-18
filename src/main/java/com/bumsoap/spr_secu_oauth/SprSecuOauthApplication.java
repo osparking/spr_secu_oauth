64323e6fc3ef65d9dc10e372502d8d265bcd2184
박종범 <jbpark03@gmail.com>
2025-04-18 13:53:57 +0900

    initial - added 8 dependencies

---
+++ src/main/java/com/bumsoap/spr_secu_oauth/SprSecuOauthApplication.java
@@ -0,0 +1,13 @@
+package com.bumsoap.spr_secu_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprSecuOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprSecuOauthApplication.class, args);
+	}
+
+}
